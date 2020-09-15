b2dcbf4d8ae051b66f36cd749f22adb43a3c1551
Strayko <strayko_lol@windowslive.com>
2020-09-15 11:25:35 +0200

    initial commit

---
+++ TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class HtmlFormatterTests
+    {
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseTheStringWithStrongElement()
+        {
+            var formatter = new HtmlFormatter();
+
+            var result = formatter.FormatAsBold("abd");
+            
+            // Specific 
+            Assert.That(result, Is.EqualTo("<strong>abd</strong>").IgnoreCase);
+            
+            // More general
+            // Assert.That(result, Does.StartWith("<strong>").IgnoreCase);
+            // Assert.That(result, Does.EndWith("</strong>"));
+            // Assert.That(result, Does.Contain("abd"));
+        }
+    }
+}
---
+++ TestNinja.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestNinja.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestNinja.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3094c34c-22cf-4526-8b3f-b54893794eca")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
