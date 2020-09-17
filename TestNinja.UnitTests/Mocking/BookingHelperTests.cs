fe9c8e0865167324f508c4ffb4f5605da427f71c
Strayko <strayko_lol@windowslive.com>
2020-09-17 11:48:06 +0200

    Extracting IBooking Repository

---
+++ TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class BookingHelperOverlappingBookingsExistTests
+    {
+        [Test]
+        public void BookingStartsAndFinishesBeforeAnExistingBooking_ReturnEmptyString()
+        {
+            
+        }
+    }
+}
